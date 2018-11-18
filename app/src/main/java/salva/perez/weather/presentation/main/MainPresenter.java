339171bb7cd8ee8672e97bd92cd05970984073aa
Salva <salvador@apelucy.com>
2018-11-18 17:50:50 +0100

    ask again after cancel permission

--- app/src/main/java/salva/perez/weather/presentation/main/MainPresenter.java
+++ app/src/main/java/salva/perez/weather/presentation/main/MainPresenter.java
@@ -94,9 +94,10 @@ public class MainPresenter extends Presenter<MainPresenter.View> implements Main
 
     public void recentPermissionResult(int[] grantResults) {
         // If request is cancelled, the result arrays are empty.
-        if (grantResults.length > 0
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             getUserLocationPermission();
+        }else{
+            requestGPSPermission();
         }
     }
 
